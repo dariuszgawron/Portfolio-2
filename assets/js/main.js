5abf98fda63e2098aef5a64798b7c4ca7683cf72
Dariusz Gawron <screbsy+g1@gmail.com>
2022-04-14 22:16:52 +0200

    Poprawki struktury, przygotowanie stylów sekcji

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// MENU
+const sections = document.querySelectorAll('.section');
+const navLinks = document.querySelectorAll('.nav__link');
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', link => {
+        navLink.classList.add('nav__link--active');
+    })
+})
