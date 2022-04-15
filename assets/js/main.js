b97f53af67a89d8b2997c52b01668075b79498af
Dariusz Gawron <screbsy+g1@gmail.com>
2022-04-15 21:21:15 +0200

    Preparation of section styles + corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const sections = document.querySelectorAll('.section');
 const navLinks = document.querySelectorAll('.nav__link');
 navLinks.forEach(navLink => {
     navLink.addEventListener('click', link => {
+        
         navLink.classList.add('nav__link--active');
     })
 })
