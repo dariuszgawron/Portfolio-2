7808f69fac7e1b4cf4f8846b0bf25030692f6823
Dariusz Gawron <screbsy+g1@gmail.com>
2022-04-16 22:12:13 +0200

    Preparation of section styles, corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 // MENU
-const sections = document.querySelectorAll('.section');
+// const sections = document.querySelectorAll('.section[id]');
 const navLinks = document.querySelectorAll('.nav__link');
 navLinks.forEach(navLink => {
-    navLink.addEventListener('click', link => {
-        
+    navLink.addEventListener('click', () => {
+        sectionId = navLink.getAttribute('data-section');
+        console.log(sectionId);
+        document.querySelector('.section--active').classList.remove('section--active');
+        document.querySelector('.nav__link--active').classList.remove('nav__link--active');
+        document.querySelector('.section[id*='+sectionId+']').classList.add('section--active');
         navLink.classList.add('nav__link--active');
     })
 })
