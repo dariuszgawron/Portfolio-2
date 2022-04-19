3c69bfa03c2aff346422a86e5ab4ea56506fb5fb
Dariusz Gawron <screbsy+g1@gmail.com>
2022-04-19 22:04:53 +0200

    Improving styles, adding animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,44 @@ navLinks.forEach(navLink => {
         document.querySelector('.section[id*='+sectionId+']').classList.add('section--active');
         navLink.classList.add('nav__link--active');
     })
-})
+});
+
+// SWIPER JS
+const swiperTestimonial = new Swiper('.swiper-testimonial', {
+    loop: true,
+    spaceBetween: 20,
+    grabCursor: true,
+    // pagination: {
+    //     el: '.swiper-testimonial__pagination',
+    //     clickable: true
+    // },
+    breakpoints: {
+        576: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 3
+        }
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    }
+});
+
+// MIXITUP
+const mixitupPortfolio = mixitup('.portfolio__container',{
+    selectors: {
+        target: '.portfolio__card'
+    },
+    animation: {
+        duration: 400
+    }
+});
+const filterLinks = document.querySelectorAll('.portfolio__filters-item');
+function changeActiveFilterLink() {
+    const className = 'button--active';
+    document.querySelector(`.${className}`).classList.remove(`${className}`);
+    this.classList.add(`${className}`);
+}
+filterLinks.forEach(filterLink => filterLink.addEventListener('click',changeActiveFilterLink));
