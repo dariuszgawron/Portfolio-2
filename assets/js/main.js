10317dc0462c18b3980848c36b53617476d93fb4
Dariusz Gawron <screbsy+g1@gmail.com>
2022-04-20 22:07:20 +0200

    Improving styles, links, adding animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
-// MENU
-// const sections = document.querySelectorAll('.section[id]');
-const navLinks = document.querySelectorAll('.nav__link');
+// MENU & LINKS
+const navLinks = document.querySelectorAll('[data-section]');
+function changeActiveSection(sectionId) {
+    document.querySelector('.section--active').classList.remove('section--active');
+    document.querySelector('.nav__link--active').classList.remove('nav__link--active');
+    document.querySelector('.section[id*='+sectionId+']').classList.add('section--active');
+    document.querySelector('.nav__link[data-section*='+sectionId+']').classList.add('nav__link--active');
+}
 navLinks.forEach(navLink => {
     navLink.addEventListener('click', () => {
-        sectionId = navLink.getAttribute('data-section');
-        console.log(sectionId);
-        document.querySelector('.section--active').classList.remove('section--active');
-        document.querySelector('.nav__link--active').classList.remove('nav__link--active');
-        document.querySelector('.section[id*='+sectionId+']').classList.add('section--active');
-        navLink.classList.add('nav__link--active');
+        changeActiveSection(navLink.getAttribute('data-section'));
     })
+});
+document.addEventListener("DOMContentLoaded", function(){
+    const sectionId = window.location.hash.substring(1) || null;
+    if(sectionId)
+        changeActiveSection(sectionId);
 });
 
 // SWIPER JS
